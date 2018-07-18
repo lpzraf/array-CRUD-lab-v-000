6b6665c910fdde3db401705095d5b3b855abb6f9
Rafael Lopez <lpz.raf@gmail.com>
2018-07-18 02:13:45 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  box = []
 end
 
 def create_an_array
-  
+  box = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  box = [1,2,3,4]
+  box.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    box = [1,2,3,4]
+    box.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    box = [1,2,3,4]
+    box.pop
 end
 
 def remove_element_from_start_of_array(array)
